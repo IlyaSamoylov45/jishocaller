544352c957cfcd3057fb021bce2394d1b883b739
Ilya <ilyasamoylov45@gmail.com>
2019-07-25 17:53:13 -0400

    added ability to specify tags

--- lib/jishocaller.ex
+++ lib/jishocaller.ex
@@ -1,18 +1,52 @@
 defmodule JISHOCALLER do
   def search(word) do
-    result = url_for(word)
-    |> HTTPoison.get([], [timeout: 10_000, recv_timeout: 10_000])
+    url_for(word)
+    |> recieve_result(word)
+  end
+
+  def search(word, tags) do
+    tags = merge_tags(tags) |> URI.encode_www_form
+    url_for(word) <> tags
+    |> recieve_result(word)
+  end
+
+  defp recieve_result(result, word) do
+    result = HTTPoison.get(result, [timeout: 10_000, recv_timeout: 10_000])
     |> parse_json
     case result do
       {:error, _} ->
         result
       _ ->
-        %{word => parse_result(result)}
+        %{word => result}
     end
+  end
+
+  defp merge_tags(tags) do
+    Stream.map(tags, &(check_string(&1)))
+    |> Stream.map(&(String.trim(&1)))
+    |> Stream.map(&(add_hashcode(&1)))
+    |> Enum.reduce(fn x, acc -> acc <> x end)
+  end
+
+  defp check_string(tag) when is_binary(tag) do
+    tag
+  end
+
+  defp check_string(_) do
+    ""
+  end
+
+  defp add_hashcode("") do
+    ""
+  end
+
+  defp add_hashcode(tag) do
+    " #" <> tag
   end
 
   defp url_for(word) do
     word = URI.encode(word)
+    IO.puts(word)
     "https://jisho.org/api/v1/search/words?keyword=#{word}"
   end
 
@@ -54,7 +88,4 @@ defmodule JISHOCALLER do
     data
   end
 
-  defp parse_result(result) do
-    result
-  end
 end
